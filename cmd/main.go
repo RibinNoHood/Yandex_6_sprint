4cce9723c1b2ec0e27fe1b9d6deb7103f620b97a
rnhqw <rnhqwerty@gmail.com>
2025-08-25 14:15:22 +0300

    first-commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+	logger := log.New(os.Stdout, "Server: ", log.LstdFlags|log.Lshortfile)
+	newServer := server.LaunchServer(logger)
+	err := server.StartServer(newServer)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
 }
