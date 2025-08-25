09efad54aad4fecd462b57680035dabaec2dd01f
rnhqw <rnhqwerty@gmail.com>
2025-08-25 14:34:02 +0300

    two-commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,55 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HtmlHandler(w http.ResponseWriter, r *http.Request) {
+
+	file, err := os.ReadFile("../index.html")
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(file)
+
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "ошибка при получении файла", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	textFromFile, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	msg := service.ConvertMsg(string(textFromFile))
+
+	dst, err := os.Create(string(time.Now().UTC().Format("2006-01-02_15-04-05")) + filepath.Ext(""))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.WriteString(dst, msg)
+	if err != nil {
+		return
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,39 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type ServerMy struct {
+	Loger  *log.Logger
+	Server http.Server
+}
+
+func LaunchServer(logger *log.Logger) *ServerMy {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlers.HtmlHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	serv := &ServerMy{
+		Loger: logger,
+		Server: http.Server{
+			Addr:         ":8080",
+			Handler:      router,
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		},
+	}
+	return serv
+}
+
+func StartServer(s *ServerMy) error {
+	err := http.ListenAndServe(s.Server.Addr, s.Server.Handler)
+	return err
+}
